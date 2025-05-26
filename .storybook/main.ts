fa5cf04a31ec59f77a572ccb6887f0e00c5fdfb3
Yulia Butyrskaya <yulia.butyrskaya@strv.com>
2025-05-26 18:26:54 +0200

    sb updates

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,17 +2,29 @@ import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
     "@storybook/experimental-addon-test",
     "@storybook/addon-postcss",
+    "@storybook/addon-mdx-gfm"
   ],
+
   framework: {
     name: "@storybook/experimental-nextjs-vite",
     options: {},
   },
+
   staticDirs: ["../public"],
+
+  docs: {
+    autodocs: true
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
